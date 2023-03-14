71da7b112d89e8c973bccc81f5121342766ff78f
Diamante710 <diamante710@icloud.com>
2023-03-14 15:06:36 -0600

    inital folder/file commits & psuedo coding

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+// bring in dependencies
+
+// configure middleware - declare apps.use etc.
+
+// Routes
+    // Homepage
+        // eg app.get('/')
+        // send back index.html
+    // Notes
+        // eg app.get('/notes')
+    
+    // Notes api
+        // app.delete('/api/notes/:noteId')
+        // 
+
+        // app.post('/api/notes')
+        // 
+
+        // app.get('/api/notes')
+        // 
