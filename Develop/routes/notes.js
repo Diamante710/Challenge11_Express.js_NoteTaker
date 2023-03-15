324c72fe0a12674cbde3af18dc488299d9fbe7f8
Diamante710 <diamante710@icloud.com>
2023-03-15 14:06:42 -0600

    lets get this functional

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,33 +1,40 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
 notes.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 notes.post('/', (req, res) => {
-    const { email, feedbackType, feedback } = req.body;
+  const { title, text } = req.body;
+  console.log(req.body)
+  if (title && text) {
+    const newFeedback = {
+      title,
+      text,
+      id: uuidv4(),
+    };
 
-    if (email && feedbackType && feedback) {
-      const newFeedback = {
-        email,
-        feedbackType,
-        feedback,
-        feedback_id: uuidv4(),
-      };
-  
-      readAndAppend(newFeedback, './db/db.json');
-  
-      const response = {
-        status: 'success',
-        body: newFeedback,
-      };
-  
-      res.json(response);
-    } else {
-      res.json('failure');
-    }
-  });
-  
-  module.exports = notes;
+    readAndAppend(newFeedback, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json('failure');
+  }
+});
+
+notes.delete('/:id', (req, res) => {
+  readFromFile('./db/db.json').then((data) => {
+    const saveNote = JSON.parse(data)
+    const filteredNotes = saveNote.filter(note => note.id !== req.params.id)
+    writeToFile('./db/db.json', filteredNotes)
+    res.json("Note Deleted")
+  })
+})
+module.exports = notes;
